aff4b3021f0c7c5bb78288cb7660bcf52a8264ac
Tomislav Bakic <bakic.tomislav@gmail.com>
2025-08-13 20:18:14 +0200

    Update next.config.ts

--- menu-qr/next.config.ts
+++ menu-qr/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    async headers() {
+    return [
+      {
+        source: '/:path*', // applies to everything
+        headers: [
+          { key: 'Cache-Control', value: 'no-store, no-cache, must-revalidate, proxy-revalidate' },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
